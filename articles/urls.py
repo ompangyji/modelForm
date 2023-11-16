d5773589237e62badc80dafacdcfafd19599521d
ompangyji <ompangyji@gmail.com>
2023-11-16 17:17:43 +0900

    내용 추가

--- articles/urls.py
+++ articles/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'articles'
 
+#유지보수랑 재사용을 위해서 아래와 같은 방식으로 작성한다.
 urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create, name='create'),
